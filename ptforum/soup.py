4a9afce2b7ffc3e28ee684b63ad6f3398e65d196
Colin Hogben <colin@pythontech.co.uk>
2012-07-29 20:45:01 +0100

    No special sys.path

--- ptforum/soup.py
+++ ptforum/soup.py
@@ -1,16 +1,13 @@
 #=======================================================================
-#       $Id: soup.py,v 1.1 2010/06/09 14:20:11 pythontech Exp $
 #	Use BeautifulSoup to make sense of not-quite-HTML pages
 #=======================================================================
-import sys
-sys.path.append('/home/pythontech/lib/python')
 import BeautifulSoup as BS
 
 BeautifulSoup = BS.BeautifulSoup
 
 def tagclass(name,klass):
     '''Create a matcher for BeautifulSoup find or findAll etc.
-    which matched a tag of given name and of the given class'''
+    which matches a tag of given name and of the given class'''
     return (lambda tag:
 	    tag.name==name and
 	    klass in tag.get('class','').split())
