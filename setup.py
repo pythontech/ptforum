5bf600c7c810ab5086b743963bb21f824e86718b
Colin Hogben <colin@pythontech.co.uk>
2012-08-01 08:41:46 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='ptforum',
+      version='0.1',
+      description='Screen-scrape forum posts and send as email',
+      author='Colin Hogben',
+      author_email='ptforum@pythontech.co.uk',
+      url='https://github.com/pythontech/ptforum',
+      packages=['ptforum'],
+      )
